c138b6543e70619067656614876d045e31fdaa4c
Alex Zhang <azhang12345@gmail.com>
2025-02-15 14:41:43 -0500

    chore: cleanup formatting

--- shared/include/event.h
+++ shared/include/event.h
@@ -12,8 +12,6 @@ struct Event
 
     static const char *StaticName() { return T::Name; };
 };
-
-// struct MouseDownEvent : public Event<MouseDownEvent> {
 struct MouseDownEvent
 {
     static constexpr const char *Name = "MouseDownEvent";
@@ -22,7 +20,6 @@ struct MouseDownEvent
     uint32_t color;
     MouseDownEvent(int x, int y, uint32_t color) : x(x), y(y), color(color) {};
 };
-// struct CanvasUiUpdateEvent : public Event<CanvasUiUpdateEvent> {
 struct CanvasUiUpdateEvent
 {
     static constexpr const char *Name = "CanvasUiUpdateEvent";
@@ -30,7 +27,6 @@ struct CanvasUiUpdateEvent
     uint32_t color;
     CanvasUiUpdateEvent(int x, int y, uint32_t color) : x(x), y(y), color(color) {}
 };
-// struct CanvasUiBatchUpdateEvent : public Event<CanvasUiBatchUpdateEvent> {
 struct CanvasUiBatchUpdateEvent
 {
     static constexpr const char *Name = "CanvasUIBatchUpdateEvent";
@@ -38,7 +34,6 @@ struct CanvasUiBatchUpdateEvent
     uint32_t color;
     CanvasUiBatchUpdateEvent(int x, int y, uint32_t color) : x(x), y(y), color(color) {}
 };
-// struct CanvasLocalUpdateEvent : public Event<CanvasLocalUpdateEvent> {
 struct CanvasLocalUpdateEvent
 {
     static constexpr const char *Name = "CanvasLocalUpdateEvent";
@@ -48,57 +43,11 @@ struct CanvasLocalUpdateEvent
     CanvasLocalUpdateEvent(int x, int y, uint32_t color, int seq) : x(x), y(y), color(color), seq(seq) {}
 };
 
-// struct CanvasServerUpdateEvent : public Event<CanvasServerUpdateEvent> {
 struct CanvasServerUpdateEvent
 {
     static constexpr const char *Name = "CanvasServerUpdateEvent";
     int x, y;
     uint32_t color;
     int seq;
-
     CanvasServerUpdateEvent(int x, int y, uint32_t color, int seq) : x(x), y(y), color(color), seq(seq) {}
 };
-
-/*
-
-using EventData = std::variant<
-    std::monostate,
-    EventMouseDown,
-    EventServerStateConflict,
-    EventServerStateUpdate,
-    EventCanvasUpdate,
-    EventClientStateUpdate>;
-
-
-enum struct EventType
-{
-    // Network Events
-    // NetworkPacketRecv,
-    // NetworkUpdateErr, // Rollback / sync error
-    ServerStateUpdate,   // authoritative Update
-    ServerStateConflict, // authoritative Update
-                         // ServerStateUpdate, // Authoritative Update
-
-    ClientStateUpdate,
-
-    // Input Events
-    MouseDown,
-    // MouseUp,
-
-    // Rendering
-    CanvasUpdate, // Pixel update applied
-                  // FrameRendered,
-                  // RedrawRequest,
-
-    // Replication & State Sync
-    // StateSyncStart,          // State synchronization process begins
-    // StateSyncComplete,       // State synchronization process ends
-    // ReplicationPacketSent,   // Replication packet was sent
-    // ReplicationPacketReceived, // Replication packet was received
-
-    // // Application Lifecycle
-    // AppInitialized,          // Application finished initialization
-    // ShutdownRequested,       // Application received shutdown signal
-    // FatalError               // Unrecoverable application error
-
-};*/
--- shared/include/event_dispatch.h
+++ shared/include/event_dispatch.h
@@ -5,29 +5,29 @@
 #include <unordered_map>
 #include <vector>
 #include <functional>
-// #include <spdlog/spdlog.h>
 #include <utility>
 #include <log.h>
-
 
 template <typename Te>
 using EventPtr = std::shared_ptr<const Event<Te>>;
 
 // Default + Specialization
 template <typename Tl, typename Te, typename = void>
-struct HasHandleEvent : std::false_type {};
+struct HasHandleEvent : std::false_type
+{
+};
 template <typename Tl, typename Te>
-struct HasHandleEvent<Tl, Te, std::void_t<
-    // decltype(std::declval<Listener>().handle_event(std::declval<Event<EventType>>()))
-    decltype(std::declval<Tl>().handle_event(std::declval<EventPtr<Te>>()))
->> : std::true_type {};
-// Template
+struct HasHandleEvent<Tl, Te, std::void_t<decltype(std::declval<Tl>().handle_event(std::declval<EventPtr<Te>>()))>> : std::true_type
+{
+};
+
+// Templated
 template <typename Tl, typename Te>
 constexpr bool HasHandleEvent_v = HasHandleEvent<Tl, Te>::value;
 
-
-template <typename Te, typename ...Args>
-inline EventPtr<Te> make_event(Args&&... args) {
+template <typename Te, typename... Args>
+inline EventPtr<Te> make_event(Args &&...args)
+{
     return std::make_shared<const Event<Te>>(std::forward<Args>(args)...);
 }
 
@@ -39,34 +39,29 @@ public:
     template <typename Te>
     void add_listener(std::function<void(EventPtr<Te>)> callback)
     {
-        SLOG("EventDispatch::Notifier::add_listener()");
-        SLOG("Listener added: {} -> {}", typeid(EventPtr<Te>).name(), typeid(Derived).name());
-
         m_listeners<Te>.emplace_back(callback);
     }
 
-        template <typename Te>
-        void notify_listeners(EventPtr<Te> e)
+    template <typename Te>
+    void notify_listeners(EventPtr<Te> e)
+    {
+        auto &l = m_listeners<Te>;
+        if (l.size() == 0)
         {
-            SLOG("EventDispatch::notify_listeners() for {}", typeid(Te).name());
-
-            auto& l = m_listeners<Te>;
-            SLOG("* Total listeners: {}", l.size());
-            if (l.size() == 0) {
-                SLOG("NO HANDLERS REGISTERED");
-            }
-
-            for (auto &callback : m_listeners<Te>)
-            {
-                SLOG("* listener");
-                callback(e);
-            }
+            SLOG("EventNotifier::notify_listeners(): no handlers registered");
         }
+
+        for (auto &callback : m_listeners<Te>)
+        {
+            callback(e);
+        }
+    }
 
 private:
     template <typename Te>
     inline static std::vector<std::function<void(EventPtr<Te>)>> m_listeners;
 };
+
 // Global add_listener Helper
 template <typename Te, typename Tn, typename Tl>
 extern inline void add_listener(Tn *notifier, Tl *listener)
@@ -76,13 +71,10 @@ extern inline void add_listener(Tn *notifier, Tl *listener)
     static_assert(HasHandleEvent_v<Tl, Te>,
                   "Listener must have a handle_event(const Event<EventType>&) function");
 
-    SLOG("Global::add_listener()");
-
+    SLOG("Registering Listeners...");
     SLOG("* Notifier -> {}", typeid(Tn).name());
-
     SLOG("* Listener -> {}", typeid(Tl).name());
 
-    // add event callback
     notifier->template add_listener<Te>([listener](EventPtr<Te> event)
                                         { listener->handle_event(event); });
 }
